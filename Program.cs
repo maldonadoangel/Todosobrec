f040e688cb7d99f3438c8ab1b97c01a88fb65471
maldonadoangel <maldonadoangel7069@gmail.com>
2021-03-06 17:41:52 -0600

    Parametros Opcionales

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace MetodosParametrosOpcionales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int valor1 = 7;
+            double valor2 = 6.3;
+            double valor3 = 7.8;
+            Console.WriteLine(Suma(valor1, valor2));
+        }
+         
+         static double Suma(int numero1, double numero2, double numero3 = 0)
+        {
+            return numero1 + numero2 + numero3;
+        }
+
+        //El metodo que llamaria es este debido a que se adapta a los parametros que solicita la funcion main
+        static double Suma(int numero1, double numero2)
+        {
+            return numero1 + numero2;
         }
     }
 }
